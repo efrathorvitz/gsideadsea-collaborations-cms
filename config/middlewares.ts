d6b64c6b727e43317d96aee630b239aa2f0f8d14
lL <118486423+efrathorvitz@users.noreply.github.com>
2025-05-14 12:54:18 +0300

    change miduler

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,51 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+import { Strapi } from '@strapi/strapi';
+
+const middleware = [
+  {
+    name: 'strapi::logger',
+    config: {
+      level: 'info',
+    },
+  },
+  {
+    name: 'strapi::errors',
+    config: {},
+  },
+  {
+    name: 'strapi::security',
+    config: {},
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // אפשר להחליף עם כתובת ספציפית אם צריך
+    },
+  },
+  {
+    name: 'strapi::poweredBy',
+    config: {},
+  },
+  {
+    name: 'strapi::query',
+    config: {},
+  },
+  {
+    name: 'strapi::body',
+    config: {},
+  },
+  {
+    name: 'strapi::session',
+    config: {},
+  },
+  {
+    name: 'strapi::favicon',
+    config: {},
+  },
+  {
+    name: 'strapi::public',
+    config: {},
+  },
 ];
+
+export default middleware;
