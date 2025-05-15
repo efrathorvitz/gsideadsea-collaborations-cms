ee93c81ddaf849548148d40fed3c59fa7c2d4d16
lL <118486423+efrathorvitz@users.noreply.github.com>
2025-05-15 13:07:51 +0300

    work with access to frond

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,1 @@
-export default () => ({
-    cors: {
-      enabled: true,
-      origin: [
-        'http://localhost:5173',
-        'https://dead-sea-observatory.vercel.app',
-      ],
-    },
-  });
-  
+export default () => ({});
